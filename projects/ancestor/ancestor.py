bae4abfd4b130e6c81939677739bc49c993c52ab
Mario Amaya <thefozcat@gmail.com>
2019-09-10 16:46:14 -0700

    completed ancestor.py

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -1,3 +1,43 @@
+def earliest_ancestor(ancestors, node):
+    valid_node = False
+    for parent in ancestors:
+            if parent[1] == node:
+                valid_node = True
+                break
+    if valid_node == False:
+        return -1
 
-def earliest_ancestor(ancestors, starting_node):
-    pass
+    q = []
+    q.append([node])
+    visited = set()
+
+    while len(q) > 0:
+        path = q.pop(0)
+        current = path[-1]
+    
+        if current not in visited:
+            visited.add(current)
+        
+        no_parent = False
+
+        for parent in ancestors:
+            if parent[1] == current:
+                path_copy = list(path)
+                path_copy.append(parent[0])
+                q.append(path_copy)
+                no_parent = True
+
+        if no_parent == False:
+            q.append(list(path))
+            break
+
+    earliest = []
+    
+    for path in q:
+        if len(path) > len(earliest):
+            earliest = list(path)
+        elif len(path) == len(earliest):
+            if path[-1] < earliest[-1]:
+                earliest = list(path)
+
+    return earliest[-1]
